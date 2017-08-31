706807bdcd744503854653ad56d2fbc42666e231
Unknown <mlgrier@gmail.com>
2017-08-30 22:22:24 -0500

    updated

--- Talkboy/SoundViewController.swift
+++ Talkboy/SoundViewController.swift
@@ -20,11 +20,6 @@ class SoundViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func recordTapped(_ sender: Any) {
     }
@@ -36,16 +31,5 @@ class SoundViewController: UIViewController {
     
     @IBAction func addTapped(_ sender: Any) {
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
