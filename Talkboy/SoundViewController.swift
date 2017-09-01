18ffa3c4c531e6b903a8d55478810b34e817f2cd
Unknown <mlgrier@gmail.com>
2017-08-31 22:59:12 -0500

    not working right

--- Talkboy/SoundViewController.swift
+++ Talkboy/SoundViewController.swift
@@ -7,21 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundViewController: UIViewController {
-
+    
     @IBOutlet weak var soundNameTextField: UITextField!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var addButton: UIButton!
     
+    var audioRecorder = AVAudioRecorder()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Create Audio Session
+        
+        let session = AVAudioSession.sharedInstance()
+        try? session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        try? session.overrideOutputAudioPort(.speaker)
+        try? session.setActive(true)
+        
+        // URL to save the audio
+        
+        if let basePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+            let patchCompoenents = [basePath, "audio.m4a"]
+            if let audioURL = NSURL.fileURL(withPathComponents: patchCompoenents) {
+                // Create some settings
+                
+                var settings : [String:Any] = [:]
+                settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC)
+                settings[AVSampleRateKey] = 44100.0
+                settings[AVNumberOfChannelsKey] = 2
+                
+                
+                // Create the Audio Recorder
+                
+                try? audioRecorder = AVAudioRecorder(url: audioURL, settings: settings)
+                audioRecorder.prepareToRecord()
+            }
+        }
     }
     
     @IBAction func recordTapped(_ sender: Any) {
+        audioRecorder.record()
     }
     
     
@@ -31,5 +60,5 @@ class SoundViewController: UIViewController {
     
     @IBAction func addTapped(_ sender: Any) {
     }
-
+    
 }
