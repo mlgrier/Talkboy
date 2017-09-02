a888adf4a6153d7919758e3e3d0d9a6e6ccc2873
Unknown <mlgrier@gmail.com>
2017-09-01 22:56:56 -0500

    completed

--- Talkboy/MainTableViewController.swift
+++ Talkboy/MainTableViewController.swift
@@ -7,17 +7,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MainTableViewController: UITableViewController {
+    
+    var sounds : [Sound] = []
+    var audioPlayer : AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getSounds()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,71 +28,56 @@ class MainTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    func getSounds() {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            if let tempSounds = try? context.fetch(Sound.fetchRequest()) as? [Sound] {
+                if let theSounds = tempSounds {
+                    sounds = theSounds
+                    tableView.reloadData()
+                }
+            }
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return sounds.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+        let sound = sounds[indexPath.row]
+        
+        cell.textLabel?.text = sound.name
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let sound = sounds[indexPath.row]
+        if let audioData = sound.audioData {
+        audioPlayer = try? AVAudioPlayer(data: audioData)
+          audioPlayer?.play()
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+        
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let sound = sounds[indexPath.row]
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                context.delete(sound)
+                getSounds()
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Talkboy/SoundViewController.swift
+++ Talkboy/SoundViewController.swift
@@ -88,6 +88,16 @@ class SoundViewController: UIViewController {
     
     
     @IBAction func addTapped(_ sender: Any) {
+        
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            let sound = Sound(entity: Sound.entity(), insertInto: context)
+            sound.name = soundNameTextField.text
+            if let audioURL = self.audioURL {
+                sound.audioData = try? Data(contentsOf: audioURL)
+                try? context.save()
+                navigationController?.popViewController(animated: true)
+            }
+        }
     }
     
 }
